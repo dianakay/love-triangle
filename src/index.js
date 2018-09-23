a69ddcb70f137a97bd8e69dd325a779ea1f42b2e
dianakay <dianakay333@gmail.com>
2018-09-23 03:35:58 +0300

    love-triangle

--- src/index.js
+++ src/index.js
@@ -3,5 +3,19 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var x,y,z,a;
+  var sum=0;
+  for (var i=0; i<preferences.length; i++) {
+	  x=preferences[i]-1;
+	  if (x==i) continue;
+	  y=preferences[x]-1;
+	  if (y==x) continue;
+	  z=preferences[y]-1;
+	  if (z==y) continue;
+	  a=preferences[z]-1;  
+  if (i==z) {
+	  sum=sum+1;
+  }
+  }
+  return sum / 3;
 };
